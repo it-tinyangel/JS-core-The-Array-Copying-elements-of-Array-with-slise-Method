8205c0ba218b545fa1ecd712fd32fb1042485cab
likes-front-end <137172921+it-tinyangel@users.noreply.github.com>
2023-10-24 21:10:59 +0300

    Copying of the array

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-// Function for copy of the array.
+// The function for copy of the array.
 function arrCopy(arr) {
 	return arr.slice();
 }
 
-// Creating copies of the arrays and output them to the console.
+// Call the function and write the result in a variable.
 const arr1 = arrCopy([1, 2, 3]);
 const arr2 = arrCopy([1, 2, 3, [10, 20]]);
 
+// Output the result to the console.
 console.log(`Copy of arr1:`, arr1);
 console.log(`Copy of arr2:`, arr2);
